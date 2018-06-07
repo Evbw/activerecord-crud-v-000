a9cd1f20a55afafbf0f392fd4d3f7a929e7fe7af
Everett <everett.bowline@gmail.com>
2018-06-07 02:08:00 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -17,7 +17,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = nil)
+def can_be_created_in_a_block(args = "Home Alone")
   Movie.create do |m|
     m = args
   end
