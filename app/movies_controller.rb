4ee59c46467aa22b19607f5084d1dcfd6f138f1a
Everett <everett.bowline@gmail.com>
2018-06-07 02:32:49 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,8 +19,9 @@ end
 
 def can_be_created_in_a_block(args = "Home Alone")
   Movie.create do |m|
-    m.title = args(:title)
+    m.title = args
   end
+  
 end
 
 def can_get_the_first_item_in_the_database
